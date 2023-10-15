9fcb5e1e2760d79194967bbf5c8421890f4f3071
Mysticdrew <drewhaas@gmail.com>
2023-10-15 12:12:57 -0500

    initiall commit

---
+++ forge/src/main/java/net/mysticdrew/journeymapftbteams/JourneyMapFtbTeamsForge.java
@@ -0,0 +1,13 @@
+package net.mysticdrew.journeymapftbteams;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(Constants.MOD_ID)
+public class JourneyMapFtbTeamsForge
+{
+
+    public JourneyMapFtbTeamsForge() {
+
+
+    }
+}
